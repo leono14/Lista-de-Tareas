d462f87f73f4e5522170ab85f4ae213541f41122
LEO <leonardomariomolina@gmail.com>
2023-11-23 12:40:14 -0300

    primera version lista de tareas

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App.css';
+import { TodoAdd } from './components/TodoAdd';
+import { TodoList } from './components/TodoList';
+import { useTodo } from './hooks/useTodo';
+
+function App() {
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleCompleteTodo,
+		handleUpdateTodo,
+	} = useTodo();
+
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Lista de tareas - Grupo 4 - Comisión 133</h1>
+				<div className='counter-todos'>
+					<h3>
+						Cantidad de Tareas: <span>{todosCount}</span>
+					</h3>
+					<h3>
+						Pendientes: <span>{pendingTodosCount}</span>
+					</h3>
+				</div>
+
+				<div className='add-todo'>
+					<h3>Agregar tarea a la lista</h3>
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+
+				<TodoList
+					todos={todos}
+					handleUpdateTodo={handleUpdateTodo}
+					handleDeleteTodo={handleDeleteTodo}
+					handleCompleteTodo={handleCompleteTodo}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default App;
